02007718eb199dfebc2cbc8c453006ea29b44e67
devinshade <devindanielle93@gmail.com>
2024-02-18 16:30:20 -0700

    pseudo-coded accepetance criteria

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,8 @@
+// WHEN I load the portfolio
+// THEN I am presented with a page containing a header, a section for content, and a footer
+
+// WHEN I load the portfolio the first time
+// THEN the About Me title and section are selected by default
+
+// WHEN I am presented with the Portfolio section
+// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repository
