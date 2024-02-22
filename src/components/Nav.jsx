328befeff65b8b6b81f693f2956ad7644e69a57d
devinshade <devindanielle93@gmail.com>
2024-02-21 20:35:01 -0700

    minor corrections

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav style={{backgroundColor: 'red', height: '200px'}}>
-        <Link to="/">Go HOme</Link>
+        <Link to="/">Go Home</Link>
         <Link to="/projects">Go Projects</Link>
 
     </nav>
